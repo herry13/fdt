c8bf458872ab14ed13b32199df8e0742f905a63e
Herry <herry13@gmail.com>
2012-12-04 00:01:38 +0000

    update

--- preprocess2/concurrent_generator.cc
+++ preprocess2/concurrent_generator.cc
@@ -3,6 +3,7 @@
 #include "operator.h"
 #include "axiom.h"
 #include "state.h"
+#include <map>
 
 bool op1_threat_prevail_op2(const Operator &op1, const Operator &op2) {
     for (int i = 0; i < op2.get_prevail().size(); i++) {
@@ -38,15 +39,26 @@ void enforce_op1_prevail_on_op2(Operator &op1, Operator &op2) {
     }
 }
 
+void merge_operators(Operator &op1, Operator &op2, vector<Operator> &operators) {
+    cout << "merge: " << op1.get_name() << " + " << op2.get_name() << " -- " << operators.size() << endl;
+}
+
+bool are_mutually_inclusive(Operator &op1, Operator &op2) {
+    cout << op1.get_name() << " :: " << op2.get_name() << endl;
+    // 1) both operators are not threatening each other
+    // 2) there's op1's prevail condition required by op2, and vice versa
+    return false;
+}
+
 void generate_concurrent_operators(vector<Operator> &operators,
                                    vector<Axiom> &axioms) {
 
     cout << "Detect and merge concurrent operators..." << endl;
-    string always = "verify_always";
+    string always = "verify_always ";
     vector<Operator> always_operators;
     Variable * always_variable = NULL;
     for (int i = 0; i < operators.size(); ) {
-        if (operators[i].get_name().substr(0, always.length()).compare(always) == 0) {
+        if (operators[i].get_name().compare(always) == 0) {
             if (always_variable == NULL) {
                 for (int j = 0; j < operators[i].pre_post.size(); j++) {
                     if (!operators[i].pre_post[j].is_conditional_effect) {
@@ -64,6 +76,8 @@ void generate_concurrent_operators(vector<Operator> &operators,
 
     vector<Operator> new_operators;
     vector<Operator> satisfied;
+    map< Operator*, vector<int> > groups;
+    int index = 0;
     for (int i = 0; i < operators.size(); ) {
         satisfied.clear();
         for (int j = 0; j < always_operators.size(); j++) {
@@ -73,7 +87,9 @@ void generate_concurrent_operators(vector<Operator> &operators,
         if (satisfied.size() < always_operators.size()) {
             for (int j = 0; j < satisfied.size(); j++) {
                 new_operators.push_back(operators[i]);
-                enforce_op1_prevail_on_op2(satisfied[j], new_operators[new_operators.size()-1]);
+                index = new_operators.size() - 1;
+                enforce_op1_prevail_on_op2(satisfied[j], new_operators[index]);
+                groups[ &(satisfied[j]) ].push_back(index);
             }
             operators.erase(operators.begin()+i);
         } else {
@@ -84,10 +100,22 @@ void generate_concurrent_operators(vector<Operator> &operators,
     cout << "total unmodified operators: " << operators.size() << endl;
     cout << "total new operators: " << new_operators.size() << endl;
 
+    int index1, index2;
+    for (map< Operator*, vector<int> >::iterator itr = groups.begin(); itr != groups.end(); itr++) {
+        for (int i = 0; i < itr->second.size(); i++) {
+            index1 = itr->second[i];
+            for (int j = i+1; j < itr->second.size(); j++) {
+                index2 = itr->second[j];
+                if (are_mutually_inclusive(new_operators[index1], new_operators[index2]))
+                    merge_operators(new_operators[index1], new_operators[index2], new_operators);
+            }
+        }
+    }
+
+
     for (int i = 0; i < new_operators.size(); i++) {
         operators.push_back(new_operators[i]);
     }
-
 
     for (int j = 0; j < operators.size(); j++) {
         for (int k = 0; k < operators[j].prevail.size(); ) {
@@ -112,8 +140,6 @@ void generate_concurrent_operators(vector<Operator> &operators,
                 j++;
         }
     }
-
-    //initial_state.values[always_variable] = 0;
 
     cout << "final total operators: " << operators.size() << endl;
 }
